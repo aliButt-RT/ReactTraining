13eefd9100a4aa6f343206bac26e4636d6a4ee73
Ali Ul Ali <ali.iftikhar@rolustech.com>
2023-04-27 19:04:58 +0500

    first commit

---
+++ src/Folder1/sampleFIle1.js
@@ -0,0 +1,11 @@
+function Sample(){
+    var a = "alilaslaslaps";
+    console.log( a );
+    return (
+        <div>
+            <a href="stackoverflow.com" style = {{color:'red'}}> This is Stack overflow Link</a>
+        </div>
+    );
+}
+
+export default Sample;
---
+++ src/first.js
@@ -0,0 +1,9 @@
+function First({ video }) {
+    return (
+      <div>
+        <h>This is my first react page</h>
+      </div>
+    );
+  }
+
+  export default First;
